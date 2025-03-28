0e5003a3f6fb5a2a20dad5a371673b6b76fd5cb8
Ivan Shkrobut <rtnxi08@gmail.com>
2025-03-28 12:52:21 +0200

    _validate

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,8 @@
 //         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 //         responsive: [
 
+// const { name } = require("browser-sync");
+
 //             {
 //                 breakpoint: 992,
 //                 settings: {
@@ -93,6 +95,39 @@ document.querySelector('.next').addEventListener('click', function () {
                 $('.overlay, #order').fadeIn('slow');
             })
         });
+
+        function validateForms(form) {
+            $(form).validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2
+                    },
+                    phone: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Пожалуйста, введите свое имя",
+                        minlength: jQuery.validator.format("Введите {0} символа!")
+                    },
+                    phone: "Пожалуйста, введите свой номер телефона",
+                    email: {
+                        required: "Пожалуйста, введите свою почту",
+                        email: "Неправильно введен адрес почты"
+                    }
+                }
+            });
+        };
+
+        validateForms('#consultation-form');
+        validateForms('#consultation form');
+        validateForms('#order form');
+
+        $('input[name=phone]').mask("+7 (999) 999-99-99");
 
     });
 })(jQuery);
