7a02c9c5fa7bf6de148fdf978fac30a67b04e3a5
Ivan Shkrobut <rtnxi08@gmail.com>
2025-03-29 14:45:33 +0200

    final

--- src/js/script.js
+++ src/js/script.js
@@ -29,23 +29,21 @@ const slider = tns({
     nav: true,
     responsive: {
         375: {
-            nav: true
+            nav: true,
         },
         640: {
             edgePadding: 20,
-            gutter: 20,
-            items: 1,
+            gutter: 20
 
         },
         700: {
             gutter: 30
         },
-        900: {
-            items: 1
-        },
+
         1100: {
             nav: false
-        },
+
+        }
     }
 });
 
@@ -128,6 +126,39 @@ document.querySelector('.next').addEventListener('click', function () {
         validateForms('#order form');
 
         $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+        $('form').submit(function (e) {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function () {
+                $(this).find("input").val("");
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #thanks').fadeIn('slow');
+
+                $('form').trigger('reset');
+            });
+            return false;
+        });
+
+        //Smooth scroll and pageup
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 1600) {
+                $('.pageup').fadeIn();
+            } else {
+                $('.pageup').fadeOut();
+            }
+        });
+
+        $("a[href=#up]").click(function () {
+            const _href = $(this).attr("href");
+            $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+            return false;
+        });
+
 
     });
 })(jQuery);
